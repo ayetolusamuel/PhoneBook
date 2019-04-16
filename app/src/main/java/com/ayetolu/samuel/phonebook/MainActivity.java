83e3d738776dd24ad51be692f6e50f700836bef4
ayetolusamuel <ayetolusamuel@gmail.com>
2019-04-16 06:25:43 +0100

    Initial commit

---
+++ app/src/main/java/com/ayetolu/samuel/phonebook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ayetolu.samuel.phonebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
