48d2d8cab8c45d9c50986ff5dd734adc7771cf44
ayetolusamuel <ayetolusamuel@gmail.com>
2019-04-17 02:37:04 +0100

    work on app first page.

--- app/src/main/java/com/ayetolu/samuel/phonebook/MainActivity.java
+++ app/src/main/java/com/ayetolu/samuel/phonebook/MainActivity.java
@@ -2,6 +2,8 @@ package com.ayetolu.samuel.phonebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        Toast.makeText(this, "Loading.....,check back later.", Toast.LENGTH_SHORT).show();
     }
 }
